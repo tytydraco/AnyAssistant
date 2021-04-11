73f0b96d8a1fc459b68296902719ce4afb49ad86
Tyler Nijmeh <tylernij@gmail.com>
2021-04-10 17:22:31 -0700

    Cleanup and gradle updates
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- app/src/main/java/com/draco/anyassistant/recyclers/SelectRecyclerAdapter.kt
+++ app/src/main/java/com/draco/anyassistant/recyclers/SelectRecyclerAdapter.kt
@@ -1,10 +1,8 @@
 package com.draco.anyassistant.recyclers
 
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
